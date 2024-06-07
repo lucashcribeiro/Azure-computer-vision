9d1eaf4d72722806448dae6c61cf4917b78f4dc0
lucasribeiro <lucashcribeiro@gmail.com>
2024-06-07 16:44:56 -0300

    Arrumando o vercel build

--- src/index.js
+++ src/index.js
@@ -6,6 +6,5 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.hydrate(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
+
 serviceWorker.unregister();
